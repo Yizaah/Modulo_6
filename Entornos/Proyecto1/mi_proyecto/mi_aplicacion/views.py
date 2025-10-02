7e0f8e470cdeee00ba83be056da28a71524ebf96
Yitzhak Valenzuela <yitzhak.1995.valenzuela@gmail.com>
2025-10-01 21:33:37 -0300

    con el gitignore + app

--- Entornos/Proyecto1/mi_proyecto/mi_aplicacion/views.py
+++ Entornos/Proyecto1/mi_proyecto/mi_aplicacion/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    contexto = {'mensaje': '¡Los que vamos a morir te saludan!'}
+    return render(request, 'mi_aplicacion/inicio.html', contexto)
